67bdfc794cb06cbde0c1e8785b9ad139120624a0
DESKTOP-QES7EH9\Kasun <kasunsjc@hotmail.com>
2018-10-23 10:19:14 +0530

    change the bootstrap script

--- EC2-Compute/aws-csa-autoscale.sh
+++ EC2-Compute/aws-csa-autoscale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install httpd -y
 yum update -y
-aws s3 cp s3://YOURBUCKETNAMEHERE/index.html /var/www/html/ --recursive --region YOURREGIONXX-YYYY-n 
+aws s3 cp s3://awsclassdemokasun /var/www/html/ --recursive
 service httpd start
 chkconfig httpd on
