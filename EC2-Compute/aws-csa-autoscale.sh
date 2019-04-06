4d3b0309f9eac3dd2bf5a326e2b9217e36a79958
Kasun Rajapakse <kasunsjc@hotmail.com>
2019-04-06 09:21:14 +0530

    Add Health.index to Script

--- EC2-Compute/aws-csa-autoscale.sh
+++ EC2-Compute/aws-csa-autoscale.sh
@@ -3,5 +3,6 @@
 yum update -y
 yum install -y httpd
 echo '<h1>Hello World</h1>' > /var/www/html/index.html
+echo '<h1>I am Healthy</h1>' > /var/www/html/health.html
 systemctl start httpd
 systemctl enable httpd
